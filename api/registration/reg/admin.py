c8fba0312f8fea9a5f70bda03c83dd4377d3b3b3
SeSo <sobhani.sepehr@gmail.com>
2023-08-15 16:58:10 -0700

    chore: add django admin views

--- api/registration/reg/admin.py
+++ api/registration/reg/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Facility, Operator, User, UserOperator
 
-# Register your models here.
+@admin.register(Operator)
+class OperatorAdmin(admin.ModelAdmin):
+    list_display = ('swrs_org_id', 'business_legal_name', 'english_trade_name', 'french_trade_name', 'cra_business_number')
+
+
+@admin.register(Facility)
+class FacilityAdmin(admin.ModelAdmin):
+    list_display = ('operator_id','swrs_facility_id', 'facility_name', 'facility_type', 'status', 'latitude', 'longitude')
+    
+
+@admin.register(UserOperator)
+class UserOperatorAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'operator_id', 'role', 'status')
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'user_guid')
+    
--- api/registration/registration/urls.py
+++ api/registration/registration/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('s/administration/raw/', admin.site.urls),
 ]
