725024f80f050f93740d59ba8e226a9393cf2caa
Brianna Cerkiewicz <brianna@button.is>
2023-08-16 16:02:05 -0700

    chore: create request form

--- client/app/access-request/page.tsx
+++ client/app/access-request/page.tsx
@@ -1,3 +1,27 @@
+'use client'
+
+import validator from '@rjsf/validator-ajv8';
+import Form from '@rjsf/core';
+import { RJSFSchema } from '@rjsf/utils';
+
+const schema: RJSFSchema = {
+  title: 'Organization Access Request',
+  type: 'object',
+  required: ['title'],
+  properties: {
+    title: { type: 'string', title: 'Operator Id', },
+    userId: { type: 'string', title: 'User Id', },
+  },
+};
+
+const log = (type) => console.log.bind(console, type);
+//  we will populate the form ourselves because we already know operator id and user id from fixtures. Basically, the user is just clicking a button to request
 export default function accessRequest() {
-    return (<><h1>Organization Access Request</h1><div>can you see me</div></>)
+    return (<><h1></h1>  <Form
+    schema={schema}
+    validator={validator}
+    onChange={log('changed')}
+    onSubmit={log('submitted')}
+    onError={log('errors')}
+  /></>)
  }
