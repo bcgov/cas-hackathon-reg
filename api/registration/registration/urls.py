52faa639b671fd3298422ef49f6961c447b333ce
SeSo <sobhani.sepehr@gmail.com>
2023-08-17 11:18:48 -0700

    chore: make api endpoint the default endpoint

--- api/registration/registration/urls.py
+++ api/registration/registration/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include(('registration.api_urls', 'api'), namespace='api')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api', include(('registration.api_urls', 'api'), namespace='api')),
     path('s/administration/raw/', admin.site.urls)
 ]
