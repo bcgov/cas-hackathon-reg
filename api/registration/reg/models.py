67c86c16d1cfc1edb2bfc4e50d59fe616311fbbe
Andrea Williams <andrea.williams@gov.bc.ca>
2023-08-16 17:30:27 -0700

    Added status column to Operator class

--- api/registration/reg/models.py
+++ api/registration/reg/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 class Operator (models.Model):
+    class Statuses(models.TextChoices):
+        PENDING = 'pending', 'Pending'
+        APPROVED = 'approved', 'Approved'
+        DENIED = 'denied', 'Denied'
+
     swrs_org_id = models.IntegerField()
     business_legal_name = models.CharField(max_length=1000)      
     english_trade_name = models.CharField(max_length=1000)
     french_trade_name = models.CharField(max_length=1000)
     cra_business_number = models.CharField(max_length=1000)
+    status = models.CharField(max_length=50, choices=Statuses.choices, default=Statuses.PENDING)
 
 class Facility(models.Model):
     operator_id = models.ForeignKey(Operator, on_delete=models.CASCADE, related_name='facilities')
