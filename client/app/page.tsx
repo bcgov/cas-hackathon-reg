41d222a7ed861d3c2b52a034b462440106a238c4
Brianna Cerkiewicz <brianna@button.is>
2023-08-21 08:58:16 -0700

    chore: update homepage links

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,14 +6,17 @@ export default function Home() {
     <main>
       <div>
         <h1>Welcome!</h1>
-        <ul>
-          <Link href="/access-request">Access Request</Link>
-        </ul>
-        <ul>
-          <Link href="/organizations">Organizations</Link>
-        </ul>
+        <h2>Demo-only pages:</h2>
         <ul>
           <Link href="/fake-user">Create a Fake User</Link>
+        </ul>
+        <h2>Pages an organization would use:</h2>
+        <ul>
+          <Link href="/access-request">Organization Access Request</Link>
+        </ul>
+        <h2>Pages an IRC user would use:</h2>
+        <ul>
+          <Link href="/organizations">List of Organizations</Link>
         </ul>
       </div>
     </main>
