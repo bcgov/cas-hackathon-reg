30bd5abf2c810f9e78cff05785cfe1e2abe55fc6
SeSo <sobhani.sepehr@gmail.com>
2023-08-21 16:14:53 -0700

    chore: add constants and types

--- client/app/types.ts
+++ client/app/types.ts
@@ -6,4 +6,41 @@ export interface User {
   user_guid: string;
 }
 
+export enum OrganizationStatus {
+  Pending = "pending",
+  Approved = "approved",
+  Denied = "denied",
+}
+
+export interface Organization {
+  id: number;
+  swrs_org_id: number;
+  business_legal_name: string;
+  english_trade_name: string;
+  french_trade_name: string;
+  cra_business_number: string;
+  status: OrganizationStatus;
+}
+
+export enum UserOrganizationRole {
+  Admin = "admin",
+  SuperAdmin = "superadmin",
+}
+
+export enum UserOrganizationStatus {
+  Pending = "pending",
+  Approved = "approved",
+  Rejected = "rejected",
+}
+
+export interface UserOrganization {
+  id: number;
+  user_id: number;
+  organization_id: number;
+  role: UserOrganizationRole;
+  status: UserOrganizationStatus;
+}
+
 export type UsersResponse = User[];
+export type OrganizationsResponse = Organization[];
+export type UserOrganizationsResponse = UserOrganization[];
---
+++ client/constants/apiEndpoints.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = "http://127.0.0.1:8000";
+export const USERS_ENDPOINT = "/users/";
+export const ORGANIZATIONS_ENDPOINT = "/organizations/";
+export const USER_ORGANIZATIONS_ENDPOINT = "/user_organizations/";
