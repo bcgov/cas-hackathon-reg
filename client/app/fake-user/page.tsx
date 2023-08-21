c3e92cf5db1a9fff4b40b65f4a7ed2bf8c6a5732
Brianna Cerkiewicz <brianna@button.is>
2023-08-21 13:27:42 -0700

    chore: comments about serializers

--- client/app/fake-user/page.tsx
+++ client/app/fake-user/page.tsx
@@ -159,7 +159,7 @@ export default function FakeUser() {
         });
       return;
     }
-    // brianna question for later: I don't think this fetching strategy is transactional, better options? A: later use a serializer and viewset to do this properly
+    // brianna question for later: I don't think this fetching strategy is transactional, better options? A: later use a serializer and viewset to do this properly, e.g. https://gist.github.com/Vibhu-Agarwal/8f71bbef7072f1deea05c75abfb3c941
 
     // for external user, add to User table
     fetch(userEndpoint, {
