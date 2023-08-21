03f7de69e983ee95255c2416530fab1b26e66d7a
SeSo <sobhani.sepehr@gmail.com>
2023-08-21 16:15:54 -0700

    chore: add api slices and replace existing calls with newly added api slices

--- client/redux/api/apiSlice.ts
+++ client/redux/api/apiSlice.ts
@@ -1,17 +1,71 @@
-import { UsersResponse } from "@/app/types";
+import {
+  UsersResponse,
+  OrganizationsResponse,
+  User,
+  Organization,
+  UserOrganizationsResponse,
+  UserOrganization,
+} from "@/app/types";
+import {
+  BASE_URL,
+  ORGANIZATIONS_ENDPOINT,
+  USERS_ENDPOINT,
+  USER_ORGANIZATIONS_ENDPOINT,
+} from "@/constants/apiEndpoints";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-const baseUrl = "http://127.0.0.1:8000";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl }),
-  tagTypes: ["Users"],
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  tagTypes: ["Users", "Organizations", "UserOrganizations"],
   endpoints: (builder) => ({
+    // User endpoints
     getUsers: builder.query<UsersResponse, void>({
-      query: () => "/users",
+      query: () => USERS_ENDPOINT,
+    }),
+    addUser: builder.mutation<User, Partial<User>>({
+      query: (body) => ({
+        url: USERS_ENDPOINT,
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: [{ type: "Users", id: "LIST" }],
+    }),
+    // Organization endpoints
+    getOrganizations: builder.query<OrganizationsResponse, void>({
+      query: () => ORGANIZATIONS_ENDPOINT,
+    }),
+    addOrganization: builder.mutation<Organization, Partial<Organization>>({
+      query: (body) => ({
+        url: ORGANIZATIONS_ENDPOINT,
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: [{ type: "Organizations", id: "LIST" }],
+    }),
+    // UserOrganization endpoints
+    getUserOrganizations: builder.query<UserOrganizationsResponse, void>({
+      query: () => USER_ORGANIZATIONS_ENDPOINT,
+    }),
+    addUserOrganization: builder.mutation<
+      UserOrganization,
+      Partial<UserOrganization>
+    >({
+      query: (body) => ({
+        url: USER_ORGANIZATIONS_ENDPOINT,
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: [{ type: "UserOrganizations", id: "LIST" }],
     }),
   }),
 });
 
-export const { useGetUsersQuery } = apiSlice;
+export const {
+  useGetUsersQuery,
+  useAddUserMutation,
+  useGetOrganizationsQuery,
+  useAddOrganizationMutation,
+  useGetUserOrganizationsQuery,
+  useAddUserOrganizationMutation,
+} = apiSlice;
