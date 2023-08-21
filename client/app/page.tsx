6efe6a542fbf0893f6ca98a5ea435226709aeff5
Brianna Cerkiewicz <brianna@button.is>
2023-08-21 08:50:55 -0700

    chore: add links to homepage

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,20 @@
 import Link from "next/link";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main>
       <div>
-        <h1>Welcome to the Operator Portal</h1>
-        <Link href="/access-request">Access Request</Link>
+        <h1>Welcome!</h1>
+        <ul>
+          <Link href="/access-request">Access Request</Link>
+        </ul>
+        <ul>
+          <Link href="/organizations">Organizations</Link>
+        </ul>
+        <ul>
+          <Link href="/fake-user">Create a Fake User</Link>
+        </ul>
       </div>
     </main>
   );
