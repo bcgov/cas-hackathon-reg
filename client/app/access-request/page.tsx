82ab97810716879bb258662bb0c87acf446b17d0
SeSo <sobhani.sepehr@gmail.com>
2023-08-22 08:52:29 -0700

    chore: use redux mutation instead of using fetch

--- client/app/access-request/page.tsx
+++ client/app/access-request/page.tsx
@@ -4,7 +4,7 @@ import validator from "@rjsf/validator-ajv8";
 import Form from "@rjsf/mui";
 import { RJSFSchema } from "@rjsf/utils";
 import { Fragment } from "react";
-// import { useGetUsersQuery } from "@/redux/api/apiSlice";
+import { useAddUserOrganizationMutation } from "@/redux/api/apiSlice";
 
 const schema: RJSFSchema = {
   title: "Organization Access Request",
@@ -16,26 +16,18 @@ const schema: RJSFSchema = {
     user_id: { type: "string", title: "User" },
   },
 };
-const userOrganizationEndpoint = "http://127.0.0.1:8000/user_organizations/";
 
 const log = (type: any) => console.log.bind(console, type);
 export default function AccessRequest() {
-  // const { data: users, error, isLoading } = useGetUsersQuery();
+  const [addUserOrganization, { isLoading: isAddingUserOrganization }] =
+    useAddUserOrganizationMutation();
 
-  const submitHandler = (data: any) => {
-    fetch(userOrganizationEndpoint, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data.formData),
-    })
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error2) => {
-        console.log(error2);
-      });
+  const submitHandler = async (data: any) => {
+    try {
+      await addUserOrganization(data.formData).unwrap();
+    } catch (err) {
+      log("error")(err);
+    }
   };
 
   return (
@@ -46,6 +38,7 @@ export default function AccessRequest() {
         validator={validator}
         onSubmit={submitHandler}
         onError={log("errors")}
+        disabled={isAddingUserOrganization}
       />
     </Fragment>
   );
