2a81df519633b64bed521fa7797fe1512ade8171
Brianna Cerkiewicz <brianna@button.is>
2023-08-24 08:09:10 -0700

    chore: reorder homepage links

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,10 +5,7 @@ export default function Home() {
     <main>
       <div>
         <h1>Welcome!</h1>
-        <h2>Demo-only pages:</h2>
-        <ul>
-          <Link href="/fake-user">Create a Fake User</Link>
-        </ul>
+
         <h2>Pages an organization would use:</h2>
         <ul>
           <Link href="/access-request">
@@ -29,6 +26,10 @@ export default function Home() {
         <ul>
           <Link href="/user-organizations">Manage Organization Access</Link>{" "}
           (you can approve/deny an organization here)
+        </ul>
+        <h2>Demo-only pages:</h2>
+        <ul>
+          <Link href="/fake-user">Create a Fake User</Link>
         </ul>
       </div>
     </main>
